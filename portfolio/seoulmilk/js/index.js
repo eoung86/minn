caaa6af96ab138d834414078fe9bb0764110f7f6
Your Name <you@example.com>
2025-12-18 09:38:37 +0900

    a

--- portfolio/seoulmilk/js/index.js
+++ portfolio/seoulmilk/js/index.js
@@ -10,3 +10,21 @@ tit.forEach((item) => {
     nav.classList.remove("active");
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
